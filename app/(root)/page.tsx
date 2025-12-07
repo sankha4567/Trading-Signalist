acd83242fc062eecb8a341ac4c13dabafa129db2
sankha4567 <ssankha103@gmail.com>
2025-12-07 11:28:33 +0530

    header related fix

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import { Button } from '@/components/ui/button'
+
 const Home = () => {
   return (
-    <div className='flex justify-center items-center h-screen'>
-      <Button>Click me</Button>
+    <div className='flex min-h-screen home-wrapper'>
+     Home
     </div>
   )
 }
---
+++ components/Navitems.tsx
@@ -0,0 +1,32 @@
+"use client"
+import React from 'react'
+import { NAV_ITEMS } from '@/lib/constants'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+const Navitems = () => {
+  const pathname=usePathname();
+  const isActive=(path:string)=>{
+     if(path === "/"){
+      return pathname === "/";
+     }
+     return pathname.startsWith(path);
+  }
+  return (
+   <ul className='flex flex-col sm:flex-row p-2 gap-3 sm:gap-10 font-medium'>
+
+    {
+      NAV_ITEMS.map((item,index)=>(
+        <li key={index}>
+         <Link href={item.href} className={`hover:text-yellow-500 transition-colors ${
+          isActive(item.href) ? "text-gray-100":''
+         }`}>
+         {item.title}
+         </Link>
+        </li>
+      ))
+    }
+   </ul>
+  )
+}
+
+export default Navitems
